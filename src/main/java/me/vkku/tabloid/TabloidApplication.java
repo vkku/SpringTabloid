9ec410994f8353bf52f1e4051e93e592b3b5c19a
vkku <kumar.vivekten@gmail.com>
2019-06-24 21:57:35 +0530

    Initial Commit

---
+++ src/main/java/me/vkku/tabloid/TabloidApplication.java
@@ -0,0 +1,13 @@
+package me.vkku.tabloid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabloidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabloidApplication.class, args);
+	}
+
+}
